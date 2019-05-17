15af97aa2a603d66d7a4415a563e94a1e753ef3a
Willem Olding <willemolding@gmail.com>
2019-05-17 15:38:26 +1000

    smartens up tests

--- test/index.js
+++ test/index.js
@@ -13,148 +13,74 @@ const instanceBob = Config.instance(agentBob, dna)
 const scenario = new Scenario([instanceAlice, instanceBob], {debugLog: false})
 
 
-// scenario.runTape("Can create a new game of checkers and make a move", async (t, { alice, bob }) => {
 
-//   // helpers
-//   let results = []
-//   const lastResult = (back=0) => results[results.length-1-back]
-//   const makeMove = async (agent, game_move) => {
-//     const result = await agent.callSync("main", "make_move", { game_move })
-//     results.push(result)
-//     return result
-//   }
-//   const getState = async (agent, game_address) => {
-//     const result = await agent.callSync("main", "render_state", { game_address })
-//     results.push(result)
-//     return result
-//   }
-
-//   let create_result = await alice.callSync("main", "create_game", { opponent: bob.agentId, timestamp: 0 })
-//   console.log(create_result)
-//   t.equal(create_result.Ok.length, 46)
-//   let game_address = create_result.Ok
-
-//   await makeMove(bob, {
-//     game: game_address,
-//     timestamp: 0,
-//     move_type: {MovePiece: { from: {x: 1, y: 5}, to: {x: 0, y: 4} }},
-//   })
-//   t.notEqual(lastResult().Ok, undefined)
-
-//   await getState(alice, game_address)
-//   console.log(lastResult().Ok)
-
-//   await makeMove(alice, {
-//   	game: game_address,
-//   	timestamp: 1,
-//   	move_type: {MovePiece: { from: {x: 0, y: 2}, to: {x: 1, y: 3} }},
-//   })
-//   t.notEqual(lastResult().Ok, undefined)
-
-//   await getState(alice, game_address)
-//   console.log(lastResult().Ok)
-
-//   // await makeMove(bob, {
-//   //   game: game_address,
-//   //   timestamp: 2,
-//   //   move_type: {MovePiece: { from: {x: 0, y: 0}, to: {x: 0, y: 0} }},
-//   // })
-//   // t.notEqual(lastResult().Ok, undefined)
-
-//   // await makeMove(alice, {
-//   //   game: game_address,
-//   //   timestamp: 2,
-//   //   move_type: {MovePiece: { from: {x: 0, y: 0}, to: {x: 0, y: 0} }},
-//   // })
-//   // t.notEqual(lastResult().Ok, undefined)
-
-//   // t.equal(lastResult().Ok.moves.length, 4)
-
-//   // both agents should see the same game state
-//   // t.deepEqual(await getState(bob, game_address), await getState(alice, game_address))
+// helpers
+let results = []
+const lastResult = (back=0) => results[results.length-1-back]
+const makeMove = async (agent, game_move) => {
+  const result = await agent.callSync("main", "make_move", { game_move })
+  results.push(result)
+  return result
+}
+const createGame = async (agent, opponent) => {
+  const result = await agent.callSync("main", "create_game", { opponent: opponent.agentId, timestamp: 0 })
+  results.push(result)
+  return result.Ok
+}
+const renderState = async (agent, game_address) => {
+  const result = await agent.callSync("main", "render_state", { game_address })
+  console.log(result.Ok)
+}
+const getState = async (agent, game_address) => {
+  const result = await agent.callSync("main", "get_state", { game_address })
+  results.push(result)
+  return result
+}
 
 
-//   // // finally print all the outputs
-//   // results.forEach((result, i) => {
-//   //   console.log(`${i}: ${JSON.stringify(result, null, 2)}\n`)
-//   // })
-
-// })
-
-scenario.runTape("Can create a new game of tic-tac-toe and make a move", async (t, { alice, bob }) => {
-
-  // helpers
-  let results = []
-  const lastResult = (back=0) => results[results.length-1-back]
-  const makeMove = async (agent, game_move) => {
-    const result = await agent.callSync("main", "make_move", { game_move })
-    results.push(result)
-    const state = await agent.callSync("main", "render_state", { game_address })
-    console.log()
-    console.log(state.Ok)
-    console.log()
-    return result
-  }
-  const getState = async (agent, game_address) => {
-    const result = await agent.callSync("main", "render_state", { game_address })
-    results.push(result)
-    return result
-  }
-
-  // can get the descripton of the valid moves
-  let valid_moves_result = await alice.callSync("main", "valid_moves", {})
-  console.log(JSON.stringify(valid_moves_result.Ok))
-  t.equal(valid_moves_result.Ok.length, 1)
 
 
-  let create_result = await alice.callSync("main", "create_game", { opponent: bob.agentId, timestamp: 0 })
-  console.log(create_result)
-  t.equal(create_result.Ok.length, 46)
-  let game_address = create_result.Ok
+scenario.runTape("Can create a new game of checkers and make a move", async (t, { alice, bob }) => {
 
+  let game_address = await createGame(alice, bob);
+
+  // agent 2 must go first
   await makeMove(bob, {
     game: game_address,
     timestamp: 0,
-    move_type: {Place: { pos: {x: 0, y: 0} }},
+    move_type: {MovePiece: { from: {x: 1, y: 5}, to: {x: 0, y: 4} }},
   })
-  t.notEqual(lastResult().Ok, undefined)
+  t.notEqual(lastResult().Ok, undefined, "Bob made the first move")
 
+  await renderState(alice, game_address)
 
   await makeMove(alice, {
-    game: game_address,
-    timestamp: 1,
-    move_type: {Place: { pos: {x: 1, y: 1} }},
+  	game: game_address,
+  	timestamp: 1,
+  	move_type: {MovePiece: { from: {x: 0, y: 2}, to: {x: 1, y: 3} }},
   })
-  t.notEqual(lastResult().Ok, undefined)
+  t.notEqual(lastResult().Ok, undefined, "Alice made the second move")
 
+  await renderState(alice, game_address)
 
   await makeMove(bob, {
     game: game_address,
     timestamp: 2,
-    move_type: {Place: { pos: {x: 0, y: 2} }},
+    move_type: {MovePiece: { from: {x: 5, y: 5}, to: {x: 6, y: 4} }},
   })
-  t.notEqual(lastResult().Ok, undefined)
+  t.notEqual(lastResult().Ok, undefined, "Bob made the third move")
+
+  let state = await getState(alice, game_address)
+
+  t.equal(state.Ok.moves.length, 3, "There were three moves in the game")
+
+  // both agents should see the same game state
+  t.deepEqual(await getState(bob, game_address), await getState(alice, game_address), "Alice and Bob both see the same game state")
 
 
-  await makeMove(alice, {
-    game: game_address,
-    timestamp: 2,
-    move_type: {Place: { pos: {x: 2, y: 0} }},
-  })
-  t.notEqual(lastResult().Ok, undefined)
-
-
-  await makeMove(bob, {
-    game: game_address,
-    timestamp: 2,
-    move_type: {Place: { pos: {x: 0, y: 1} }},
-  })
-  t.notEqual(lastResult().Ok, undefined)
-
-
-  // finally print all the outputs
-  results.forEach((result, i) => {
-    console.log(`${i}: ${JSON.stringify(result, null, 2)}\n`)
-  })
+  // // finally print all the outputs
+  // results.forEach((result, i) => {
+  //   console.log(`${i}: ${JSON.stringify(result, null, 2)}\n`)
+  // })
 
 })

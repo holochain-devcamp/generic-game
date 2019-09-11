06ecca3590646fca72bfbe491b71e2814a7691de
guillem.cordoba <guillem.cordoba@gmail.com>
2019-09-11 19:25:28 +0200

    Extended comments

--- zomes/main/code/src/your-game/state.rs
+++ zomes/main/code/src/your-game/state.rs
@@ -58,8 +58,21 @@ impl GameState {
          * DEVCAMP TODO #4:
          * Return the initial game state
          * 
-         * Hint: 
-         * References: 
+         * Hint: this function will always return a static value, that 
+         * Example: 
+         *     GameState {
+         *         moves: Vec::new(),
+         *         player_1: PlayerState {
+         *             pieces: Vec::new(),
+         *             resigned: false,
+         *             winner: false,
+         *         },
+         *         player_2: PlayerState {
+         *             pieces: Vec::new(),
+         *             resigned: false,
+         *             winner: false,
+         *         },
+         *     }
          */
     }
 
@@ -70,8 +83,8 @@ impl GameState {
          * 
          * Hint: useful snippets:
          *     let mut disp = "\n".to_string();
-         *     disp.push_str("  x  0 1 2 3 4 5 6 7\ny\n");
-         * References: 
+         *     disp.push_str("  x  0 1 2 3 4 5 6 7\n");
+         * References: https://doc.rust-lang.org/rust-by-example/std/str.html
          */
     }
 
@@ -84,9 +97,63 @@ impl GameState {
          * Hints: 
          *   - This is similar to a Redux reducer, a function that given a state and a new action,
          *     returns the next state
-         *   - You can declare helper functions (eg: get_current_player, format_state)
-         *     to help transform your game state into an easier format for this function to read
-         * References: 
+         *   - It can be really helpful to use the helper functions below to transform your state before and after
+         *     you apply the next_move changes to it. For example, the function could be structured like this:
+         *       * Transform GameState to a matrix of all pieces
+         *       * Apply move to the matrix
+         *       * Transform matrix to GameState
+         * References: https://www.joshmcguigan.com/blog/array-initialization-rust/
          */
     }
 }
+
+/*========================================
+=            Helper functions            =
+
+// This function transforms the game state from a vector of pieces to a matrix where each
+// position represents the location of the piece
+
+// Visual representation:
+// From: { player1: [{x: 0, y: 4}, {x: 6, y: 3}], player2: [{x: 6, y: 4}, {x: 5, y: 6}]
+// To: 
+// [
+//    [0, 0, 0, 0, 1, 0, 0, 0],
+//    [0, 0, 0, 0, 0, 0, 0, 0],
+//    [0, 0, 0, 0, 0, 0, 0, 0],
+//    [0, 0, 0, 0, 0, 0, 0, 0],
+//    [0, 0, 0, 0, 0, 0, 0, 0],
+//    [0, 0, 0, 0, 0, 0, 2, 0],
+//    [0, 0, 0, 1, 2, 0, 0, 0],
+//    [0, 0, 0, 0, 0, 0, 0, 0],
+//]
+
+pub fn board_sparse_to_dense(state: &GameState)-> [[u8; 8]; 8] {
+    let mut board = [[0u8; 8]; 8];
+    state.player_1.pieces.iter().for_each(|piece| {
+        board[piece.x][piece.y] = 1;
+    });
+    state.player_2.pieces.iter().for_each(|piece| {
+        board[piece.x][piece.y] = 2;
+    });
+    board
+}
+
+// This function is the inverse of the function above,
+// takes a matrix in the same format of above and transforms it to a tuple of two vectors of pieces,
+// that can directly be saved in the entry
+
+pub fn board_dense_to_sparse(board: [[u8; 8]; 8]) -> (Vec<Piece>, Vec<Piece>) {
+    let mut player_1_pieces = Vec::new();
+    let mut player_2_pieces = Vec::new();
+    board.iter().enumerate().for_each(|(x, row)| {
+        row.iter().enumerate().for_each(|(y, square)| {
+            if *square == 1 {
+                player_1_pieces.push(Piece{x , y});
+            } else if *square == 2 {
+                player_2_pieces.push(Piece{x , y});               
+            }
+        })
+    });
+    (player_1_pieces, player_2_pieces)
+}
+========================================*/
